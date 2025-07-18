72083aae48b1e2acfa119f67b89cd788a6b06d8a
Apran Khunger <85071754+aprankhunger@users.noreply.github.com>
2025-07-18 18:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (99.97%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,9 +1,22 @@
-// #include <cmath.h>
-
-#include <cmath>
 class Solution {
 public:
     int mySqrt(int x) {
-    return sqrt(x);
+        int start=0,end = x,mid,ans=1;
+        if(x==0||x==1)
+        return x;
+        while(start<=end){
+            mid = (start+end)/2;
+            if(mid==x/mid){
+                return mid;
+            }else if(mid<x/mid){
+                ans=mid;
+                start = mid+1;
+            }else{
+                // ans=mid;
+                end =mid -1;
+            }
+        }
+        return ans;
+        
     }
 };
